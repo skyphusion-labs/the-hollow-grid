1b298e02202c9b2be47bc8ff21c734bff819b903
Conrad Rockenhaus (SkyPhusion) <conrad@rockenhaus.net>
2026-06-16 15:46:08 -0500

    Reduced vitest time to an appropriate time/removed workerd (not needed)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    testTimeout: 1000000, 
-    coverage: {
+    testTimeout: 250000, 
+    coverage: {  
       provider: 'istanbul',
       reporter: ['cobertura'],
       reportsDirectory: './coverage'
