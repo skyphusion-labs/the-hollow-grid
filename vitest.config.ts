ceb336c29b7ba4ebb09a79e7b6ce1e787f9bd579
Conrad Rockenhaus (SkyPhusion) <conrad@rockenhaus.net>
2026-06-16 14:32:52 -0500

    Trying multi-worker test again

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,25 +3,19 @@ import { cloudflareTest } from '@cloudflare/vitest-pool-workers';
 
 export default defineConfig({
   plugins: [
-    cloudflareTest()
-  ],
-  test: {
-    poolOptions: {
-      workers: {
-        wrangler: { configPath: './wrangler.jsonc' },
-        
-        isolatedStorage: true,
-        auxiliaryWorkers: [
-          {
-            name: 'grid-hub',
-            configPath: './grid-hub/wrangler.jsonc'
-          },
-          {
-            name: 'dustfall',
-            configPath: './worlds/dustfall.jsonc'
-          }
-        ]
-      }
-    }
-  }
+    cloudflareTest({
+      wrangler: { configPath: './wrangler.jsonc' },
+      
+      auxiliaryWorkers: [
+        {
+          name: 'grid-hub',
+          configPath: './grid-hub/wrangler.jsonc'
+        },
+        {
+          name: 'dustfall',
+          configPath: './worlds/dustfall.jsonc'
+        }
+      ]
+    })
+  ]
 });
