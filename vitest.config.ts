e1654a4f41e9d7d8fedaa857fa25604a076b2d3f
Conrad Rockenhaus (SkyPhusion) <conrad@rockenhaus.net>
2026-06-16 14:04:58 -0500

    Fixed typos in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import { cloudflareTest } from '@cloudflare/vitest-pool-workers/config';
+import { cloudflareTest } from '@cloudflare/vitest-pool-workers'; 
 
 export default defineConfig({
   plugins: [
