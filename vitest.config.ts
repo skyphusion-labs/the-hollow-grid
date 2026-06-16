3ad9f8f478c3f36a57ff4f478a2faccb468490ae
Conrad Rockenhaus (SkyPhusion) <conrad@rockenhaus.net>
2026-06-16 15:10:05 -0500

    Final test attempt

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,18 +4,22 @@ import { cloudflareTest } from '@cloudflare/vitest-pool-workers';
 export default defineConfig({
   plugins: [
     cloudflareTest({
-      wrangler: { configPath: './wrangler.jsonc' },
-      isolatedStorage: true,
-      auxiliaryWorkers: [
-        {
-          name: 'grid-hub',
-          configPath: './grid-hub/wrangler.jsonc'
-        },
-        {
-          name: 'dustfall',
-          configPath: './worlds/dustfall.jsonc'
-        }
-      ]
+      wrangler: { 
+        configPath: './wrangler.jsonc',
+        
+        services: [
+          {
+            binding: 'GRID',
+            service: 'grid-hub',
+            configPath: './grid-hub/wrangler.jsonc'
+          },
+          {
+            service: 'dustfall',
+            configPath: './worlds/dustfall.jsonc'
+          }
+        ]
+      },
+      isolatedStorage: true
     })
   ],
   test: {
