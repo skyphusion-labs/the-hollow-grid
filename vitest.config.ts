b1661f1f08d3863b17712b7b4f4a8b62bbc06342
Conrad Rockenhaus (SkyPhusion) <conrad@rockenhaus.net>
2026-06-16 15:30:56 -0500

    Added more time to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    testTimeout: 100000, 
+    testTimeout: 1000000, 
     coverage: {
       provider: 'istanbul',
       reporter: ['cobertura'],
